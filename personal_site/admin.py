c3f3d64c6454279362d33599723351e8f074c378
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-05-05 14:40:12 -0700

    New Models made along with template to display them.

--- personal_site/admin.py
+++ personal_site/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from personal_site.models import ExampleItem
+from personal_site.models import ExampleItem, ImageModel
 
 # Register your models here.
 admin.site.register(ExampleItem)
+admin.site.register(ImageModel)
