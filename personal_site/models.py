f3a877432ed7b3513b4542f8eda198f45c5c07e1
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-05-05 12:09:02 -0700

    Added Test Model and more defined layout

--- personal_site/models.py
+++ personal_site/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class ExampleItem(models.Model):
+    title = models.CharField(max_length=30)
+    summary = models.TextField()
+    img = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
