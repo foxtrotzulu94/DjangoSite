fba632eeba19848438f5893a6a7dd28e76a21ca8
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-05-05 17:45:41 -0700

    A lot of changes made to layout
    Still need to make the sections load dynamically after the page loads. Javascript and jQuery have been ok so far. Hopefully the honeymoon lasts...

--- personal_site/models.py
+++ personal_site/models.py
@@ -4,13 +4,15 @@ from django.core.files.storage import FileSystemStorage
 # path = os.path.join(ROOT_DIR, 'web-private')
 # fs = FileSystemStorage(location='/static/personal_site/images/')
 
+
 class ImageModel(models.Model):
+    """Class for linking a Model to a list of Images"""
     img = models.ImageField(upload_to='media/')
 
     def __str__(self):
         return self.img.name+" - ("+str(self.img.width)+"x"+str(self.img.height)+")"
-
 # end class ImageModel
+
 
 class ExampleItem(models.Model):
     title = models.CharField(max_length=30)
@@ -21,6 +23,7 @@ class ExampleItem(models.Model):
 
     def __str__(self):
         return self.title
+# end class
 
 
 class DisplayItem(models.Model):
@@ -37,5 +40,7 @@ class ExperienceItem(DisplayItem):
 
     tech_used = models.CharField(max_length=200)
     description = models.TextField()
-    # TODO: list of images?
-    pass
+    display_pictures = models.ManyToManyField(ImageModel)
+# end class
+
+
