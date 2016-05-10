e055893907aab96e1a6710028eb42b5aa999ff49
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-05-09 23:13:31 -0700

    Models code and DB schema complete. First pass on REST API
    All models are now done and the database schema has been finished. REST API is on its way, though the current implementation is quite monolithic. There is a way to make it nicer by having each file within the 'rest' package (name not final) make a urlpatterns list that can be included and mapped to specific elements. Even better would be to have a more well defined interface class in python to which to map the classes can be mapped.

--- personal_site/site_urls.py
+++ personal_site/site_urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url
-from personal_site.views import *
-
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
-# from django.http.response import HttpResponseForbidden
+
+from personal_site.views import *
 
 urlpatterns = [
     url(r'^$', index),
+
+    # Testing URLs, comment out for release
     url(r'^testy/', testy),
     url(r'^ajaj/', ajaj),
-    # url(r'^/', HttpResponseForbidden)
-    url(r'^data/projects', list_projects),
-    # url(r'^data/projects/?P<project>[A-z]{4}[0-9]{3}',), # TODO: fill this in
-    # url(r'^data/games'),
-    # url(r'^data/interests')
+
+    url(r'', include('personal_site.rest.urls')),
+    url(r'^example/projects/', list_projects),
+
+    # ImageListField Query
+    url(r'^images/?P<img>[0-9]*$', image_field_retrieval),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
