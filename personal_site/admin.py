67a10eb90221a74540286b5b19351224a9c33464
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-05-15 20:19:07 -0700

    Modified views to move away from AJAX.

--- personal_site/admin.py
+++ personal_site/admin.py
@@ -2,16 +2,25 @@ from django.contrib import admin
 from personal_site.models import *
 
 # Register your models here.
-
 # Example Models
 admin.site.register(ExampleItem)
 admin.site.register(ImageListField)
 
-# Actual Models that are displayed throughout the site
-admin.site.register(WorkExperience)
-admin.site.register(VolunteerExperience)
-admin.site.register(ExtracurricularExperience)
-admin.site.register(PersonalProject)
-admin.site.register(GameTitle)
-admin.site.register(PersonalInterest)
 
+# Actual Models that are displayed throughout the site
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'external_link')
+# end class
+
+
+@admin.register(WorkExperience, ExtracurricularExperience, VolunteerExperience)
+class ExperienceItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organization', 'location', 'start_date', 'end_date', 'ongoing')
+# end class
+
+
+@admin.register(PersonalProject, GameTitle, PersonalInterest)
+class GeneralDisplayItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'ongoing')
+# end class
