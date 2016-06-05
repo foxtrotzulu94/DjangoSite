9e6f2568b313752660144028b0f4a99a99cb1faa
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-06-05 16:16:50 -0700

    Adding VirtualEnv for reliable operation

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/DMZ/Personal/Projects/Website/foxtrot94/copy of domain/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
