2f3ebd272da444544faca7ced6a21fa65700043b
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-06-05 19:37:41 -0700

    Fixing typo

--- domain/settings.py
+++ domain/settings.py
@@ -134,4 +134,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
