4af70d343621adf73af7ee616e077fe6570ecc3c
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2017-05-18 01:49:52 -0400

    Deleted virtualenv for good!

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/DMZ/Personal/Projects/Website/foxtrot94/copy of domain/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
