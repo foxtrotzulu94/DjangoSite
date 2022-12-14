fea2db6666f5eceb21d911987d31cc52883b17e9
Javier E. Fajardo <javier@fajardo.io>
2022-12-13 23:34:27 -0800

    Make args not optional to catch mistakes

--- runDocker.ps1
+++ runDocker.ps1
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker kill personal_site
 docker rm personal_site
-docker build -t personal_site .
+docker build -t personal_site . --build-arg ENV_DEBUG=1
 docker run -p8000:7654 -v ${PSScriptRoot}/media:/app/media --name personal_site personal_site "python" "manage.py" "runserver" "0.0.0.0:7654"
