912081421d901e064e680cd795b47a0974f2cefd
Javier E. Fajardo <javier@fajardo.io>
2022-12-16 00:02:58 -0800

    Add some gunicorn options

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@ python manage.py migrate --noinput
 if [[ -z $DEBUG || $DEBUG -eq 0 ]];
 then
     echo Running production server on port 8000
-    gunicorn --bind localhost:8000 domain.wsgi:application
+    gunicorn --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread --bind localhost:8000 domain.wsgi:application
 else
     echo Running debug server on port 7654
     python manage.py runserver 0.0.0.0:7654
